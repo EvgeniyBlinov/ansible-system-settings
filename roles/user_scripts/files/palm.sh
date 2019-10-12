d604e4fcaad405d9f217dd4dba6aedba934862df
Blinov Evgeniy <evgeniy_blinov@mail.ru>
2019-10-12 15:17:38 +0300

    master
    Work stage 2019-10-12 15:17:38
    A  roles/user_scripts/files/palm.sh
    ?? disable-services.yml

---
+++ roles/user_scripts/files/palm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+        #Synaptics Palm Detection (318): 0
+        #Synaptics Palm Dimensions (319):        10, 200
+xinput set-prop 14 "Synaptics Palm Detection" 1
+#xinput set-prop 14 "Synaptics Palm Dimensions" 3, 3
+xinput set-prop 14 "Synaptics Palm Dimensions" 3, 200
